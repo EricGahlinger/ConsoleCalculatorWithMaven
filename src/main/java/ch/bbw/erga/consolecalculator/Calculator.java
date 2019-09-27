0d4b90eaedfbcfd4eda7ebd30e6e0d9a1ff300e5
eric.gahlinger <eric.gahlinger@lernende.bbw.ch>
2019-09-27 14:48:58 +0200

    Implemented

---
+++ src/main/java/ch/bbw/erga/consolecalculator/Calculator.java
@@ -0,0 +1,13 @@
+package ch.bbw.erga.consolecalculator;
+
+/**
+ * 
+ * @author eric.gahlinger
+ * @version 27.09.2019
+ */
+
+public class Calculator {
+	public int summe(int summand1, int summand2)  {
+		return summand1+summand2;
+	}
+}
